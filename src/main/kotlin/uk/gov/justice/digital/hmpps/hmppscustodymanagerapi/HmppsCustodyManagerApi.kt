c2665c8e70cb227ba6e2c4e43aec65ee9f222bce
David Winchurch <david.winchurch@digital.justice.gov.uk>
2024-01-26 16:35:05 +0000

    CMG-14: Manually apply template file changes

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppscustodymanagerapi/HmppsCustodyManagerApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppscustodymanagerapi/HmppsCustodyManagerApi.kt
@@ -3,7 +3,7 @@ package uk.gov.justice.digital.hmpps.hmppscustodymanagerapi
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication()
+@SpringBootApplication
 class HmppsCustodyManagerApi
 
 fun main(args: Array<String>) {
